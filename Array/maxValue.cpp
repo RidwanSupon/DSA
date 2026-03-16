f05eddac3e0ee83a76b7af7ab3111680d2270e6b
Md. Ridwanur Rahman Mazumder <ridwansupon@gmail.com>
2026-03-16 13:14:51 +0600

    array

--- Array/maxValue.cpp
+++ Array/maxValue.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr[] = {12, 7, 16, 7, 6, 21, 2};
     int n = 7;
-    int maxVal = 0;
+    int maxVal = arr[0];
     for(int i=1; i<n; i++){
         if(maxVal < arr[i]){
             maxVal = arr[i];
